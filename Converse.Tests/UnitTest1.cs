af270ca5744edfe2f22affd30dbd512cfd53591a
JaredMathis <jared.mathis@native.consulting>
2022-12-16 10:51:17 -0500

    refactor

--- Converse.Tests/UnitTest1.cs
+++ Converse.Tests/UnitTest1.cs
@@ -3,19 +3,26 @@ namespace Converse.Tests
     [TestClass]
     public class UnitTest1
     {
+        private string startWord;
+        private Learner sut;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            startWord = "apple";
+            sut = new Learner(startWord);
+        }
+
         [TestMethod]
         public void TestMethod2()
         {
-            var word = "apple";
-            var l = new Learner(word);
-            Assert.AreEqual(l.Learn(), Learner.AskDefinitionFor(word));
         }
+
         [TestMethod]
         public void TestMethod1()
         {
-            var word = "apple";
-            var l = new Learner(word);
-            Assert.AreEqual(l.Learn(), Learner.AskDefinitionFor(word));
+            var l = new Learner(startWord);
+            Assert.AreEqual(l.Learn(), Learner.AskDefinitionFor(startWord));
         }
     }
 }
