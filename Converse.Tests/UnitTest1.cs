3324e84bbbe698502c3603638dfff727da8e2841
JaredMathis <jared.mathis@native.consulting>
2022-12-16 10:49:07 -0500

    copy method

--- Converse.Tests/UnitTest1.cs
+++ Converse.Tests/UnitTest1.cs
@@ -4,6 +4,13 @@ namespace Converse.Tests
     public class UnitTest1
     {
         [TestMethod]
+        public void TestMethod2()
+        {
+            var word = "apple";
+            var l = new Learner(word);
+            Assert.AreEqual(l.Learn(), Learner.AskDefinitionFor(word));
+        }
+        [TestMethod]
         public void TestMethod1()
         {
             var word = "apple";
